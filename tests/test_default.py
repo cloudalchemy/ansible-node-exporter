1261c390996051015d5973a55d4e89f8877fe8f2
Paweł Krupa <pkrupa@sointeractive.pl>
2017-11-09 10:47:29 +0100

    change way of enabling collectors

--- tests/test_default.py
+++ tests/test_default.py
@@ -3,33 +3,33 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_files(File):
+def test_files(host):
     present = [
         "/etc/systemd/system/node_exporter.service",
         "/opt/node_exporter"
     ]
     if present:
         for file in present:
-            f = File(file)
+            f = host.file(file)
             assert f.exists
             assert f.is_file
 
 
-def test_service(Service):
+def test_service(host):
     present = [
         "node_exporter"
     ]
     if present:
         for service in present:
-            s = Service(service)
+            s = host.service(service)
             assert s.is_enabled
             assert s.is_running
 
 
-def test_socket(Socket):
+def test_socket(host):
     present = [
         "tcp://127.0.0.1:9100"
     ]
     for socket in present:
-        s = Socket(socket)
+        s = host.socket(socket)
         assert s.is_listening
