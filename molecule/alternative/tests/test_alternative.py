138daa71a9f2db2619d6e0e86ec76cad76c09292
Paweł Krupa <pawel@krupa.net.pl>
2018-12-16 16:21:30 +0100

    add alternative tests (#65)

---
+++ molecule/alternative/tests/test_alternative.py
@@ -0,0 +1,29 @@
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_directories(host):
+    dirs = [
+        "/var/lib/node_exporter"
+    ]
+    for dir in dirs:
+        d = host.file(dir)
+        assert not d.exists
+
+
+def test_service(host):
+    s = host.service("node_exporter")
+#    assert s.is_enabled
+    assert s.is_running
+
+
+def test_socket(host):
+    sockets = [
+        "tcp://127.0.0.1:9100"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
